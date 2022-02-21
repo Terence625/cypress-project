d2f9c9a728dbdfc20178b6b4f68666c74d2241ad
Terence625 <terencetianterence@gmail.com>
2022-02-21 18:53:25 +0800

    [Wenguang] add material ui package and a basic text field component to test

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Input from "./component/Input";
-
+import { StyledEngineProvider } from '@mui/material/styles';
+import BasicTextFields from "./component/BasicTextFields";
 // ========================================
 
-ReactDOM.render(<Input />, document.getElementById("root"));
-
+ReactDOM.render(
+  <StyledEngineProvider injectFirst>
+    <BasicTextFields />
+  </StyledEngineProvider>,
+  document.querySelector("#root")
+);
